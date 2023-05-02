816a0538041dcf3de8e604a9cad6c5b3884ec461
Ahmed <ahmed@Ahmeds-MacBook-Air.local>
2023-05-02 13:53:48 +0100

    email url fix

--- resources/views/emails/appointment-created.blade.php
+++ resources/views/emails/appointment-created.blade.php
@@ -6,7 +6,7 @@ Hello {{ $staffName }},
 A visitation appointment for **{{ $appointment->first_name.' '.$appointment->last_name }}** from **{{ $appointment->company }}** has been created for you.
 Click the button below to *accept* or *reject* the appointment. 
 
-<x-mail::button :url="'http://localhost:8000/my-appointments'">
+<x-mail::button :url="'https://vms.nmdpra.gov.ng/my-appointments'">
 View Appointment
 </x-mail::button>
 
